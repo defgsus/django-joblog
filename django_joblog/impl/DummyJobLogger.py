d86afaa2bc589c01a1ea7faea28bbca21cc5b522
bergi <s.berke@netzkolchose.de>
2019-04-29 22:11:07 +0200

    remove left-over code relict

--- django_joblog/impl/DummyJobLogger.py
+++ django_joblog/impl/DummyJobLogger.py
@@ -54,29 +54,3 @@ class DummyJobLogger(JobLoggerBase):
             print("ERR: %s" % line)
         except (UnicodeDecodeError, UnicodeEncodeError):
             pass
-
-
-class JobLoggerContext(object):
-    """
-    Scoped context change for a Logger instance
-
-    with JobLogger("my_task") as log:
-        with JobLoggerContext(log, "subtask_1"):
-            the_task_1(log)
-        with JobLoggerContext(log, "subtask_2"):
-            the_task_2(log)
-            with JobLoggerContext(log, "subsubtask_2"):
-                the_sub_task_2(log)
-    """
-    def __init__(self, log, name):
-        self._log = log
-        self._name = name
-
-    def __enter__(self):
-        self._log.push_context(self._name)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if not exc_tb:
-            self._log.pop_context()
-
